1ffae93e7e9d4455e64a6a034181b259501201bb
abaabiil <ibnujokar@hotmail.com>
2023-07-26 12:00:43 +0300

    added some nice future

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ const min = date.getMinutes();
 const hour = date.getHours();
 
 const label = (document.querySelector(".date").textContent = `${hour}:${min}`);
+
+const facebook = document.querySelector(".facebook");
+const fbIcon = document.querySelector(".fb");
+const swap = document.querySelector(".swap");
+
+fbIcon.addEventListener("click", function () {
+  facebook.classList.remove("hide");
+});
+
+swap.addEventListener("click", function () {
+  facebook.classList.add("hide");
+});
