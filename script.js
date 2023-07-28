6d1006de404276a46054e92c77ab3904f6403a36
abaabiil <ibnujokar@hotmail.com>
2023-07-28 16:54:01 +0300

    updated facebook

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
 "use strict";
 
 const date = new Date();
-const min = date.getMinutes();
-const hour = date.getHours();
+const min = date.getMinutes().toString().padStart(2, 0);
+const hour = date.getHours().toString().padStart(2, 0);
 
 const label = (document.querySelector(".date").textContent = `${hour}:${min}`);
-
 const facebook = document.querySelector(".facebook");
 const fbIcon = document.querySelector(".fb");
-const swap = document.querySelector(".swap");
+const arrow = document.querySelector(".arrow-back");
 
 fbIcon.addEventListener("click", function () {
   facebook.classList.remove("hide");
+  arrow.style.visibility = "visible";
 });
 
-swap.addEventListener("click", function () {
+arrow.addEventListener("click", function (e) {
   facebook.classList.add("hide");
+  setTimeout(function () {
+    arrow.style.visibility = "hidden";
+  }, 1000);
 });
